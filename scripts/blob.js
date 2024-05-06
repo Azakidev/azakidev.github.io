81730d8ccdcbeedd23bf1806f8944f56e123e084
Azakidev <zazaguichi@outlook.com>
2024-05-07 01:09:22 +0200

    Landpage 2.0

--- scripts/blob.js
+++ scripts/blob.js
@@ -1,4 +1,5 @@
 const blob = document.getElementById("blob");
+let image = document.getElementById("image");
 
 document.body.onpointermove = event => {
     const {clientX, clientY} = event
@@ -7,6 +8,27 @@ document.body.onpointermove = event => {
         left: `${clientX}px`,
         top: `${clientY}px`,
     }, {duration: 1500, fill: "forwards"});
+
+    //Image rotation on the landing page is handled here, why? Javascript is weird
+
+    if ( image != undefined ) {
+
+        let rect = image.getBoundingClientRect();
+
+        let centX = (rect.x + (rect.width / 2)),
+            centY = (rect.y + (rect.height / 2));
+
+        let wX = (clientX - centX) / document.body.clientWidth,
+            wY = (clientY - centY) / document.body.clientHeight;
+
+        let compX = Math.min(0.45, wY) *  0.5,
+            compY = Math.min(0.45, wX) * -0.8,
+            compZ = Math.min(0.15, wY) * -0.2;
+
+        image.animate({
+            transform: `rotate3d( ${compX}, ${compY}, ${compZ}, 15deg)`
+        }, {duration: 1200, fill: "forwards"});
+    }
 }
 
 document.body.ontouchmove = event => {
@@ -15,5 +37,5 @@ document.body.ontouchmove = event => {
     blob.animate({
         left: `${clientX}px`,
         top: `${clientY}px`,
-    }, {duration: 3000, fill: "forwards"});
+    }, {duration: 1500, fill: "forwards"});
 }
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,15 +3,12 @@ const layout = document.getElementsByClassName("layout")[0];
 const cards = document.getElementsByClassName("cards")[0];
 
 function scrollhandler() {
-  let opacity = window.getComputedStyle(arrow).getPropertyValue("opacity");
-  if (opacity != 0) {
-    layout.scrollIntoView({behavior: 'smooth' })
-  }
+    let opacity = window.getComputedStyle(arrow).getPropertyValue("opacity");
+    if (opacity != 0) {
+        layout.scrollIntoView({behavior: 'smooth' })
+    }
 }
 
 function cardsscrollhandler() {
-  let opacity = window.getComputedStyle(arrow).getPropertyValue("opacity");
-  if (opacity != 0) {
     cards.scrollIntoView({behavior: 'smooth' })
-  }
 }
